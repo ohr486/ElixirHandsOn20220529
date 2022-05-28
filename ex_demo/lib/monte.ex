f622c5f311b948ccd6c14cf7fb91aaf9031621d7
ohr486 <ohr486@gmail.com>
2022-05-29 08:54:00 +0900

    add monte

--- ex_demo/lib/monte.ex
+++ ex_demo/lib/monte.ex
@@ -1,28 +1,29 @@
 defmodule ExDemo.Monte do
+  @try 1000000 # 試行数
+
+  # 円周率計算
   def try1 do
-    # 試行数
-    n = 100000000
-
     start_time = :os.system_time(:millisecond) # 計算開始
-    pi = monte_method(n)
+    pi = monte_method1(@try)
     end_time = :os.system_time(:millisecond) # 計算終了
-
     diff = end_time - start_time
-    IO.puts("calc_time:#{diff}msec")
-    IO.puts("pi:#{inspect pi}")
+
+    IO.puts("試行数:#{@try}")
+    IO.puts("計算時間:#{diff}msec")
+    IO.puts("円周率:#{inspect pi}")
   end
 
-  def monte_method(n) do
-    monte_method(n, n, 0)
+  def monte_method1(n) do
+    monte_method1(n, n, 0)
   end
 
-  defp monte_method(0, n, point) do
+  defp monte_method1(0, n, point) do
     4.0 * point / n
   end
 
-  defp monte_method(i, n, point) do
+  defp monte_method1(i, n, point) do
     new_point = check_new_point(point) 
-    monte_method(i-1, n, new_point)
+    monte_method1(i-1, n, new_point)
   end
 
   defp check_new_point(point) do
@@ -33,5 +34,33 @@ defmodule ExDemo.Monte do
     else
       point
     end
+  end
+
+  # nxを使った円周率計算
+  def try2 do
+    start_time = :os.system_time(:millisecond) # 計算開始
+    pi = monte_method2(@try)
+    end_time = :os.system_time(:millisecond) # 計算終了
+    diff = end_time - start_time
+
+    IO.puts("試行数:#{@try}")
+    IO.puts("計算時間:#{diff}msec")
+    IO.puts("円周率:#{inspect pi}")
+  end
+
+  def monte_method2(n) do
+    x = Nx.random_uniform({n}, 0, 1, type: {:f, 32})
+    y = Nx.random_uniform({n}, 0, 1, type: {:f, 32})
+
+    # x*x+y*y
+    xxyy = Nx.add(Nx.multiply(x,x),Nx.multiply(y,y))
+
+    # 要素が1.0より大きい場合は0に変換
+    less1 = Nx.less(xxyy, 1.0)
+
+    # 1.0より小さい要素数
+    point = Nx.sum(less1) |> Nx.to_flat_list |> Enum.at(0)
+
+    4.0 * point / n
   end
 end
