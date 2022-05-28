49741091af81fdc2b1c8da3ff04c1b536f5694d4
ohr486 <ohr486@gmail.com>
2022-05-29 06:29:29 +0900

    add ex02

--- ex_demo/lib/ex_demo.ex
+++ ex_demo/lib/ex_demo.ex
@@ -1,18 +1,17 @@
 defmodule ExDemo do
-  @moduledoc """
-  Documentation for `ExDemo`.
-  """
+  # sample2
+  def ex2 do
+    IO.puts "--- ex.02 ---"
 
-  @doc """
-  Hello world.
+    # 1x1ベクトル
+    IO.inspect(Nx.tensor(10))
 
-  ## Examples
+    # 1x3ベクトル
+    IO.inspect(Nx.tensor([1,2,3]))
 
-      iex> ExDemo.hello()
-      :world
+    # 2x3ベクトル
+    IO.inspect(Nx.tensor([[1,2,3],[4,5,6]]))
 
-  """
-  def hello do
-    :world
+    nil
   end
 end
